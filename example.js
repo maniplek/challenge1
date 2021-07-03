41f0c637371e7f4c7407d2a6e63404a61507508e
Maniple Kwizera <83705949+maniplek@users.noreply.github.com>
2021-07-03 08:37:47 +0200

    added fetch posts feature

--- example.js
+++ example.js
@@ -1,34 +1,40 @@
-function mani(){
-    fetch("https://jsonplaceholder.typicode.com/users")
-     .then(response =>{
-         if(!response.ok) {
-             throw Error("ERROR");
-
-         }
-         return response.json();
-     })
-     .then(data => {
-         console.log(data.data);
-         const html = data.data
-           .map(user =>{
-             return `
-             <div class="user">
-                 <p>Name: ${user.name}</p>
-                 <p>Email: ${user.email}</p>
-             </div>
+function mani() {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response) => {
+      if (!response.ok) {
+        throw Error("ERROR");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const html = data
+        .map((user) => {
+          return `
+                <p>Name: ${user.name}</p>
+                <p>Email: ${user.email}</p>
+                <button onclick="getPosts(${user.id})">Get User's post</button>    
              `;
-         })
-         .join("");
-         document.querySelector("#app").insertAdjacentHTML("afterbegin",html);
-     })
-     .catch(error =>{
-         console.log(error);
-     });
+        })
+        .join("");
+      document.querySelector(".user").insertAdjacentHTML("afterbegin", html);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 mani();
-
-// function fetchData(){
-//      fetch('https://jsonplaceholder.typicode.com/users');
-//     //console.log("Start Fetch");
-// }
-// fetchData();
+function getPosts(id) {
+  fetch(`http://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    .then((response) => response.json())
+    .then((json) => {
+      const content = json
+        .map((post) => {
+          return `
+          <p>post title: ${post.title}</p>
+          <p>post title: ${post.body}</p>
+             `;
+        })
+        .join("");
+      document.querySelector(".posts").innerHTML = content;
+    });
+}
